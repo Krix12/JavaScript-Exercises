81c1e15761e631a6ad81ff08073da822e621b62c
Krix12 <65765469+Krix12@users.noreply.github.com>
2022-01-19 23:15:21 +0100

    Create Solution.js

---
+++ Very Easy/01/Solution.js
@@ -0,0 +1,7 @@
+const input = [2, 3];
+
+const output = input[0] +input[1];
+//output[0] is the first element(number) in array
+//output[1] is the second element(number) in array
+console.log(output)
+//OUTPUT: 5
